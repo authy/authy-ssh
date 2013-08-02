6469c26343a1c5bf41415e4ae78599562d94510d
David A. Cuadrado <krawek@gmail.com>
2013-08-02 11:38:06 -0500

    Prevent symlink race issues when updating the plugin.
    This is done by randomizing the temp file using the command `mktemp`
    Thanks to R.I.Pienaar for reporting this issue.

--- tests/test_update.rb
+++ tests/test_update.rb
@@ -11,3 +11,43 @@ authy_ssh("update") do |stdin, stdout|
   end
 end
 
+FileUtils.cp(AUTHY_SCRIPT, "#{AUTHY_SCRIPT}.backup")
+mtime = File.stat(AUTHY_SCRIPT).mtime
+authy_ssh("update", {}, true) do |stdin, stdout|
+  print "Updating the file"
+
+  if read_until(stdout, /Do you want to overwrite .*\?/)
+    stdin.puts "y"
+  end
+
+  if read_until(stdout, /Now type authy-ssh test/)
+    new_mtime = File.stat(AUTHY_SCRIPT).mtime
+    if new_mtime > mtime
+      green " [OK]"
+    else
+      red " [FAILED]"
+    end
+  end
+end
+FileUtils.mv("#{AUTHY_SCRIPT}.backup", AUTHY_SCRIPT)
+
+FileUtils.cp(AUTHY_SCRIPT, "#{AUTHY_SCRIPT}.backup")
+mtime = File.stat(AUTHY_SCRIPT).mtime
+authy_ssh("update", {}, true) do |stdin, stdout|
+  print "Rejecting update"
+
+  if read_until(stdout, /Do you want to overwrite .*\?/)
+    stdin.puts "n"
+  end
+
+  if read_until(stdout, /Authy SSH was not updated./)
+    new_mtime = File.stat(AUTHY_SCRIPT).mtime
+    if new_mtime == mtime
+      green " [OK]"
+    else
+      red " [FAILED]"
+    end
+  end
+end
+FileUtils.mv("#{AUTHY_SCRIPT}.backup", AUTHY_SCRIPT)
+
